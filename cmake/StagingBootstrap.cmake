f3a3d68b5c700882d5fd3e1b04888d26a712bcdd
Aliaksandr Adziareika <8034372+alexadereyko@users.noreply.github.com>
2026-08-28 15:43:08 +0200

    Pin the cmake utils to v1.0.3

--- cmake/StagingBootstrap.cmake
+++ cmake/StagingBootstrap.cmake
@@ -9,7 +9,7 @@ if (NOT COMMAND opendaq_write_metadata)
     FetchContent_Declare(
         opendaq-cmake-utils
         GIT_REPOSITORY https://github.com/openDAQ/opendaq-cmake-utils.git
-        GIT_TAG        ci/staging
+        GIT_TAG        v1.0.3
         SOURCE_SUBDIR  _download_only_
     )
     FetchContent_MakeAvailable(opendaq-cmake-utils)
